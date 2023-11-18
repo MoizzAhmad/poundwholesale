5707638016fd047c2e2d63529912a0063865f0d2
MoizzAhmad <145003412+MoizzAhmad@users.noreply.github.com>
2023-11-18 08:58:49 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+document.getElementById("action").addEventListener("mouseover", function() {
+    document.getElementById("face-icon-text").style.color = "black";
+    document.getElementById("face-icon").style.color = "white";
+});
+document.getElementById("action").addEventListener("mouseout", function() {
+    document.getElementById("face-icon-text").style.color = " white"; // You can use an empty string or the default color
+    document.getElementById("face-icon").style.color = "#3ec3bf"; // You can use an empty string or the default color
+});
+// ************************************************************************************************
+document.getElementById("action2").addEventListener("mouseover", function() {
+    
+    document.getElementById("face-icon-text2").style.color = "black";
+
+    document.getElementById("face-icon2").style.color = "white";
+
+   
+});
+document.getElementById("action2").addEventListener("mouseout", function() {
+    document.getElementById("face-icon-text2").style.color = " white"; // You can use an empty string or the default color
+    document.getElementById("face-icon2").style.color = " #3ec3bf"; // You can use an empty string or the default color
+});
+// ************************************************************************************************
+
+document.getElementById("action3").addEventListener("mouseover", function() {
+    
+    document.getElementById("face-icon-text3").style.color = "black";
+
+    document.getElementById("face-icon3").style.color = "white";
+
+   
+});
+document.getElementById("action3").addEventListener("mouseout", function() {
+    document.getElementById("face-icon-text3").style.color = " white"; // You can use an empty string or the default color
+    document.getElementById("face-icon3").style.color = " #3ec3bf"; // You can use an empty string or the default color
+});
+// ************************************************************************************************
+
+document.getElementById("action4").addEventListener("mouseover", function() {
+    
+    document.getElementById("face-icon-text4").style.color = "black";
+
+    document.getElementById("face-icon4").style.color = "white";
+
+   
+});
+document.getElementById("action4").addEventListener("mouseout", function() {
+    document.getElementById("face-icon-text4").style.color = " white"; // You can use an empty string or the default color
+    document.getElementById("face-icon4").style.color = " #3ec3bf"; // You can use an empty string or the default color
+});
+
+// ************************************************************************************************
+document.getElementById("action5").addEventListener("mouseover", function() {
+    
+    document.getElementById("face-icon-text5").style.color = "black";
+
+    document.getElementById("face-icon5").style.color = "white";
+
+   
+});
+document.getElementById("action5").addEventListener("mouseout", function() {
+    document.getElementById("face-icon-text5").style.color = " white"; // You can use an empty string or the default color
+    document.getElementById("face-icon5").style.color = " #3ec3bf"; // You can use an empty string or the default color
+});
+/* Define the styling for the custom-slide class */
